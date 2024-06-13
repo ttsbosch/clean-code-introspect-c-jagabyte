31157ffa016c7f3bc1550163de90e662f8142733
jagabyte <mrjagatheesan@gmail.com>
2024-06-13 16:13:52 +0530

    Update TradeRecord.h

--- TradeRecord.h
+++ TradeRecord.h
@@ -1,6 +1,11 @@
+#define MAX_LINE_LENGTH 1024
+#define MAX_RECORDS 1024
+#define LOT_SIZE 100
+#define MAX_CURRENCY_NAME_SIZE 256
+
 typedef struct {
-    char sourceCurrency[4];
-    char destinationCurrency[4];
-    int lots;
-    double price;
-} TradeRecord;
+    char sourceCurrency[MAX_CURRENCY_NAME_SIZE]; 
+    char destibationCurrency[MAX_CURRENCY_NAME_SIZE];
+    float Lots;
+    double Price;
+} TradeRecord ;
