3f39dac0875284f38b0f2118f45f3240b605581d
jagabyte <mrjagatheesan@gmail.com>
2024-06-13 16:28:55 +0530

    Update ConvertCSVTradeDataToXML.cpp

--- ConvertCSVTradeDataToXML.cpp
+++ ConvertCSVTradeDataToXML.cpp
@@ -2,62 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <ConvertCSVTradeDataToXML.h>
+#include <SplitString.h>
+#include <StringHelper.h>
+#include <Converters.h>
 
-char** SplitString(const char* str, char delimiter) {
-    int count = 0;
-    const char* ptr = str;
-    while (*ptr != '\0') {
-        if (*ptr++ == delimiter) {
-            count++;
-        }
-    }
-
-    char** tokens = (char**)malloc(sizeof(char*) * (count + 2));
-    int i = 0;
-    ptr = str;
-    char* token = (char*)malloc(strlen(str) + 1);
-    int j = 0;
-    while (*ptr != '\0') {
-        if (*ptr == delimiter) {
-            token[j] = '\0';
-            tokens[i++] = strdup(token);
-            j = 0;
-        } else {
-            token[j++] = *ptr;
-        }
-        ptr++;
-    }
-    token[j] = '\0';
-    tokens[i++] = strdup(token);
-    tokens[i] = NULL;
-    free(token);
-    return tokens;
-}
-
-
-int intGetFromString(const char* str, int* value) {
-    char* endptr;
-    *value = strtol(str, &endptr, 10);
-    if (endptr == str) {
-        return 0;
-    }
-    return 1;
-}
-
-int toDouble(const char* str, double* value) {
-    char* endptr;
-    *value = strtod(str, &endptr);
-    if (endptr == str) {
-        return 0;
-    }
-    return 1;
-}
-
-void Process(FILE* stream) {
-    char line[1024];
-    TR objects[1024];
+void convertCSVTradeDataToXML(FILE* stream) {
+    char line[MAX_LINE_LENGTH];
+    TradeRecord  records[MAX_RECORDS];
     int lineCount = 0;
-    int objectCount = 0;
+    int recordCount = 0;
 
     while (fgets(line, sizeof(line), stream)) {
         char* fields[3];
@@ -78,36 +32,36 @@ void Process(FILE* stream) {
             continue;
         }
 
-        int tam;
-        if (!intGetFromString(fields[1], &tam)) {
+        int tradeAmount;
+        if (!getIntegerFromString(fields[1], &tradeAmount)) {
             fprintf(stderr, "WARN: Trade amount on line %d not a valid integer: '%s'\n", lineCount + 1, fields[1]);
         }
 
-        double tp;
-        if (!toDouble(fields[2], &tp)) {
+        double tradePrice;
+        if (!tryToConvertDouble(fields[2], &tradePrice)) {
             fprintf(stderr, "WARN: Trade price on line %d not a valid decimal: '%s'\n", lineCount + 1, fields[2]);
         }
 
-        strncpy(objects[objectCount].SC, fields[0], 3);
-        strncpy(objects[objectCount].DC, fields[0] + 3, 3);
-        objects[objectCount].Lots = tam / LotSize;
-        objects[objectCount].Price = tp;
-        objectCount++;
+        strncpy(records[recordCount].sourceCurrency, fields[0], 3);
+        strncpy(records[recordCount].destibationCurrency, fields[0] + 3, 3);
+        records[recordCount].Lots = tradeAmount / LotSize;
+        records[recordCount].Price = tradePrice;
+        recordCount++;
         lineCount++;
     }
 
     FILE* outFile = fopen("output.xml", "w");
     fprintf(outFile, "<TradeRecords>\n");
-    for (int i = 0; i < objectCount; i++) {
+    for (int recordIndex  = 0; recordIndex  < recordCount; recordIndex ++) {
         fprintf(outFile, "\t<TradeRecord>\n");
-        fprintf(outFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", objects[i].SC);
-        fprintf(outFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", objects[i].DC);
-        fprintf(outFile, "\t\t<Lots>%d</Lots>\n", objects[i].Lots);
-        fprintf(outFile, "\t\t<Price>%f</Price>\n", objects[i].Price);
+        fprintf(outFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", records[recordIndex ].sourceCurrency);
+        fprintf(outFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", records[i].destibationCurrency);
+        fprintf(outFile, "\t\t<Lots>%d</Lots>\n", records[recordIndex ].Lots);
+        fprintf(outFile, "\t\t<Price>%f</Price>\n", records[recordIndex ].Price);
         fprintf(outFile, "\t</TradeRecord>\n");
     }
     fprintf(outFile, "</TradeRecords>");
     fclose(outFile);
-    printf("INFO: %d trades processed\n", objectCount);
+    printf("INFO: %d trades processed\n", recordCount);
 }
 
